0a24873a177f38b743754af7d4aba396e4d681f4
JHES561 <thejhes2006@gmail.com>
2026-04-27 17:20:01 -0500

    mejoras

--- actividad-bootstrap/js/script.js
+++ actividad-bootstrap/js/script.js
@@ -1,46 +1,33 @@
-//botones de los card para abrir una ventana modal
-
+//botones de las cards para abrir la ventana modal
 let botones = document.querySelectorAll(".btn-modal");
 
-botones.forEach(boton =>{
- boton.addEventListener("click", function(){
-    let modal = new bootstrap.Modal(document.getElementById("miModal"));
-    modal.show();
-
- })
-
+botones.forEach(boton => {
+    boton.addEventListener("click", function(){
+        let modal = new bootstrap.Modal(document.getElementById("miModal"));
+        modal.show();
+    })
 })
-
-//validación del formulario
-
+// validacion del formulario
 document.getElementById("loginForm").addEventListener("submit", function(e){
-   e.preventDefault()
-   let email = document.getElementById("email").value
-   let password = document.getElementById("password").value
-   let message = document.getElementById("message")
+    e.preventDefault();
 
-   //valores didacticos para esta actvidad
-
-    const USER_DEFAULT = "prueba@gmail.com";
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password".value);
+    let message = document.getElementById("message");
+    //Valores didacticos para esta actividad
+    const USEER_DEFAULT = "prueba@gmail.com";
     const PASSWORD_DEFAULT = "123456";
 
-    if (email === "" || password ===""){
-        message.innerHTML = "Todo los campos son obligatorios";
+    if(email === "" || password === ""){
+        message.innerHTML = "Todos son obligatorios!";
         message.className = "text-warning";
-        
-    } else if(email ===USER_DEFAULT && password === PASSWORD_DEFAULT) {
-        
-        message.innerHTML = "Bienvenido al sistema"
-        message.className = "text-success"
-
-    }else{
-        message.innerHTML = "Error: usuario o contraseña incorrecta"
-        message.className = "text-danger"
     }
-
-
-    
+    else if (email === USER_DEFAULT && password === PASSWORD_DEFAULT){
+        message.innerHTML = "Bienvenido al sistema";
+        message.className = "text-succes";
+    }
+    else {
+        message.innerHTML = "Error : Usuario o contraseña invalidas!"
+        messages.className = "text-danger";
+    }
 })
-
-
-
